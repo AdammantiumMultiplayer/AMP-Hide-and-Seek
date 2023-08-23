2f464fc4ac27579c85f248b701d22aac5c72be43
flexhd41 <83534174+flexhd41@users.noreply.github.com>
2023-08-23 20:39:04 +0200

    Add files via upload

---
+++ app.cs
@@ -0,0 +1,76 @@
+using AMP;
+using AMP.DedicatedServer;
+using AMP.Logging;
+using AMP.Network.Data;
+using AMP.Network.Packets.Implementation;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace HideAndSeekPlugin {
+    public class HideAndSeek : AMP_Plugin {
+        public override string NAME    { get { return "HideAndSeek"; } }
+        public override string VERSION { get { return "0.0.1"; } }
+        public override string AUTHOR  { get { return "flexhd""; } }
+
+        private bool gameRunning = false;
+        private List<ClientData> hiders = new List<ClientData>();
+        private ClientData seeker = null;
+
+        public override void OnStart() {
+            Log.Info(NAME, "Hide and Seek plugin started.");
+        }
+
+        public override void OnPlayerJoin(ClientInformation client) {
+            if (!gameRunning) {
+                hiders.Add(client);
+                Log.Info(NAME, $"{client.name} joined the game as a hider.");
+            } else {
+                // You can handle joining during an ongoing game differently if needed.
+            }
+        }
+
+        public override void OnPlayerDamaged(ClientInformation player, float damage, ClientInformation damager) {
+            if (gameRunning && hiders.Contains(player)) {
+                // Player was damaged by another player, so they are caught.
+                hiders.Remove(player);
+                Log.Info(NAME, $"{player.name} was caught by {damager.name}!");
+            }
+        }
+
+        public override void OnPlayerQuit(ClientInformation client) {
+            if (gameRunning) {
+                if (client == seeker) {
+                    // Handle the case where the seeker quits during the game.
+                    // You might want to choose a new seeker here.
+                } else if (hiders.Contains(client)) {
+                    hiders.Remove(client);
+                }
+            }
+        }
+
+        public void StartGame() {
+            if (hiders.Count >= 4) {
+                gameRunning = true;
+
+                // Choose a random seeker from the hiders.
+                seeker = hiders[UnityEngine.Random.Range(0, hiders.Count)];
+                hiders.Remove(seeker);
+
+                Log.Info(NAME, $"{seeker.name} is the seeker!");
+
+                // Notify players about the game starting.
+                ModManager.serverInstance.SendReliableToAll(new DisplayTextPacket("game_start", "Hide and Seek game started!", Color.yellow, Vector3.forward * 2, true, true, 10));
+            } else {
+                Log.Info(NAME, "Not enough players to start the game.");
+            }
+        }
+
+        public void EndGame() {
+            gameRunning = false;
+            seeker = null;
+            hiders.Clear();
+            Log.Info(NAME, "Hide and Seek game ended.");
+        }
+    }
+}
